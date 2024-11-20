7a436c9abcc31b4bfd92dcb4d757379bf0e2ac53
Lucas Faria <up202207540@up.pt>
2024-11-20 18:48:07 +0000

    The right menu is now closed by default

--- resources/views/partials/context_menu.blade.php
+++ resources/views/partials/context_menu.blade.php
@@ -1,5 +1,5 @@
-<aside class = "flex fixed lg:sticky top-0 right-0 w-60 bottom-12 max-h-screen transition-all ease-in duration-300" id = "context-menu">
-    <button class = "absolute right-56 z-10 top-2 bg-sky-950 shadow rounded-3xl p-2 transition-all ease-in duration-300 overflow-scroll" onclick = "contextMenuOperation()">
+<aside class = "flex fixed lg:sticky top-0 right-0 w-0 bottom-12 max-h-screen transition-all ease-in duration-300" id = "context-menu">
+    <button class = "absolute right-56 z-10 top-2 translate-x-56 bg-sky-950 shadow rounded-3xl p-2 transition-all ease-in duration-300 overflow-scroll rotate-180" onclick = "contextMenuOperation()">
         <svg class= "fill-white rotate-180" xmlns="http://www.w3.org/2000/svg" id="Bold" viewBox="0 0 24 24" width="25" height="25">
             <path d="M10.482,19.5a1.5,1.5,0,0,1-1.06-.439L4.836,14.475a3.505,3.505,0,0,1,0-4.95L9.422,4.939a1.5,1.5,0,0,1,2.121,2.122L6.957,11.646a.5.5,0,0,0,0,.708l4.586,4.585A1.5,1.5,0,0,1,10.482,19.5Z"/>
             <path d="M17.482,19.5a1.5,1.5,0,0,1-1.06-.439l-6-6a1.5,1.5,0,0,1,0-2.122l6-6a1.5,1.5,0,1,1,2.121,2.122L13.6,12l4.939,4.939A1.5,1.5,0,0,1,17.482,19.5Z"/>
