8f00897ff5c08ef67b78b61f6ff80f517971b693
Granja5 <ze.diogo.granja@gmail.com>
2024-11-21 15:32:07 +0000

    Search Ball Opacity decrease

--- resources/views/partials/search_menu.blade.php
+++ resources/views/partials/search_menu.blade.php
@@ -27,7 +27,7 @@
 </div>
 
 <div id="search-ball" 
-    class="lg:hidden fixed top-4 right-4 w-12 h-12 bg-sky-900 rounded-full flex items-center justify-center shadow-md">
+    class="lg:hidden fixed top-1.5 right-4 w-12 h-12 bg-sky-900 rounded-full flex items-center justify-center shadow-md z-20 bg-opacity-90">
     <a href="{{ route('search') }}">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="white" stroke="white" stroke-width="0.5">
             <path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z"/>
