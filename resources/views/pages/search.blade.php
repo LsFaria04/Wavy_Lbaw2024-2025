5b104f7f3b590ead843c6dd836952f647d9a7b8b
Lucas Faria <up202207540@up.pt>
2024-11-22 21:41:12 +0000

    Back button now return to the previous page

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -4,7 +4,7 @@
         <div class="flex flex-col items-center w-full bg-white" id="searchPage">
             <header id="search-header" class="w-full max-w-full pt-4 shadow-md items-center sticky top-0 z-10 backdrop-blur">
                 <div class="flex items-center">
-                    <a href="{{ route('home') }}" class="flex items-center text-gray-500 hover:text-gray-700 mr-4 pl-4">
+                    <a href="{{ url()->previous() }}" class="flex items-center text-gray-500 hover:text-gray-700 mr-4 pl-4">
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
                         </svg>
