671775254f0b2476f558b909375aeec742e14f3d
Lucas Faria <up202207540@up.pt>
2024-11-23 21:41:02 +0000

    Added a margin to the bottom of the content section on mobile devices

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <body class="flex flex-col min-h-screen overscroll-none overflow-x-hidden">
         <main class = "flex flex-col grow lg:flex-row bg-white ">
             @include('partials.navigation_menu')
-            <section id="content" class= "grow flex flex-col">
+            <section id="content" class= "grow flex flex-col mb-16 lg:mb-0">
                 @yield('content')
                 @yield('scripts')
             </section>
