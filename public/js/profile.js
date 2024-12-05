b772877e992b994494a7b679fc848fb8b7c2b432
Lucas Faria <up202207540@up.pt>
2024-12-05 19:04:30 +0000

    Improved the add with instant removal in the add topics menu without reload2

--- public/js/profile.js
+++ public/js/profile.js
@@ -266,34 +266,8 @@ function addEventListeners() {
       console.log(topics);
       //iterate throw the topics and add them in the list
       for(let i = 0; i < topics.data.length; i++){
-        let topic = document.createElement('li');
-        topic.classList.add("w-full","flex","justify-between", "p-2", "my-2", "shadow");
-        topic.setAttribute('id',`topic-${topics.data[i].topicid}`)
-        
-        if(isMyTopics){
-          topic.innerHTML = `
-            <p class="text-gray-800 font-semibold">${topics.data[i].topicname}</p>
-            <button class="text-red-500 hover:text-red-700 ml-2">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-          `;
-        }
-        else{
-          topic.innerHTML = `
-            <p class="text-gray-800 font-semibold">${topics.data[i].topicname}</p>
-            <button onclick=addTopicToUser(${topics.data[i].topicid}) class="text-green-500 hover:text-green-700 ml-2">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="20" height="20">
-                    <g>
-                        <path d="M480,224H288V32c0-17.673-14.327-32-32-32s-32,14.327-32,32v192H32c-17.673,0-32,14.327-32,32s14.327,32,32,32h192v192   c0,17.673,14.327,32,32,32s32-14.327,32-32V288h192c17.673,0,32-14.327,32-32S497.673,224,480,224z" fill= "currentColor"/>
-                    </g>
-                </svg>
-            </button>
-          `;
-        }
-        
-        topicsList.appendChild(topic);
+        let topic = createTopic(topics.data[i], isMyTopics);
+        topicsList.appendChild(topic, isMyTopics);
       }
 
       //show the more topics button again if we found more topics. We also display a warning if no topics were found
@@ -315,6 +289,38 @@ function addEventListeners() {
       }
 
   }
+
+  //creates a new topic with a layout that depends on the page where the topic is going to be inserted
+  function createTopic(topicInfo, isMyTopics){
+    let topic = document.createElement('li');
+    topic.classList.add("w-full","flex","justify-between", "p-2", "my-2", "shadow");
+    topic.setAttribute('id',`topic-${topicInfo.topicid}`)
+    if(isMyTopics){
+      topic.innerHTML = `
+        <p id = "topic-${topicInfo.topicid}" class="text-gray-800 font-semibold">${topicInfo.topicname}</p>
+        <button class="text-red-500 hover:text-red-700 ml-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+      `;
+    }
+    else{
+      topic.innerHTML = `
+        <p id = "topic-${topicInfo.topicid}" class="text-gray-800 font-semibold">${topicInfo.topicname}</p>
+        <button onclick=addTopicToUser(${topicInfo.topicid}) class="text-green-500 hover:text-green-700 ml-2">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="20" height="20">
+                <g>
+                    <path d="M480,224H288V32c0-17.673-14.327-32-32-32s-32,14.327-32,32v192H32c-17.673,0-32,14.327-32,32s14.327,32,32,32h192v192   c0,17.673,14.327,32,32,32s32-14.327,32-32V288h192c17.673,0,32-14.327,32-32S497.673,224,480,224z" fill= "currentColor"/>
+                </g>
+            </svg>
+        </button>
+      `;
+    }
+
+    return topic;
+  }
+
 
   const html = document.documentElement;
 
@@ -382,6 +388,18 @@ function addEventListeners() {
     sendAjaxRequest('put', '/api/topics/add/' + topicId + '/' + userId, null, function(){
       let response = JSON.parse(this.responseText);
       console.log(response.response);
+
+      if(response.response == '200'){
+        //remove element from the add topics page
+        let topic = document.getElementById(`topic-${topicId}`);
+        let topicName = topic.querySelector('p').innerHTML;
+        topic.remove();
+
+        //add topic to the my topics page
+        let newTopic = createTopic({'topicname' : topicName, 'topicid' : topicId }, true);
+        let topicList = document.querySelector("#myTopicsList > ul");
+        topicList.insertBefore(newTopic, topicList.firstChild);
+      }
     });
   }
 
