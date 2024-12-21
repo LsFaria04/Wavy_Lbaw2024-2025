28bbfea3646c166f2a854b4f672b6c94b6ad3a42
Pedro Borges <up202207552@up.pt>
2024-12-21 16:42:59 +0000

    Fixed bug in personalized timeline

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -3,17 +3,15 @@
     <div class="flex flex-col items-center w-full max-w-full bg-white" id="postPage">
 
         <!-- Flash Messages -->
-        <div class="fixed top-6 flex items-center z-50">
-            @if (session('error'))
-                <div class = "self-center alert w-full max-w-full p-4 bg-red-100 text-red-800 border shadow-md text-center border-red-300 z-10">             
-                    {{ session('error')}}          
-                </div>
-            @elseif(session('success'))
-                <div class = "self-center alert w-full max-w-full p-4 bg-blue-100 text-blue-800 border shadow-md text-center border-blue-300 z-10">             
-                    {{ session('success')}}          
-                </div>
-            @endif 
-        </div>
+        @if (session('error'))
+            <div class="absolute self-center alert w-full max-w-full p-4 mb-4 bg-red-100 text-red-800 border shadow-md text-center border-red-300 rounded-lg z-10">             
+                {{ session('error') }}          
+            </div>
+        @elseif(session('success'))
+            <div class="absolute self-center alert w-full max-w-full p-4 mb-4 bg-green-100 text-green-800 border shadow-md text-center border-green-300 rounded-lg z-10">             
+                {{ session('success') }}          
+            </div>
+        @endif 
         <div id="messageContainer" class="fixed top-6 flex items-center">
             <!--Used to append messages with JS -->
         </div>
@@ -45,7 +43,7 @@
                                     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
                                         <path d="M19.828 11.244L12.707 18.364C10.755 20.317 7.589 20.317 5.636 18.364C3.684 16.411 3.684 13.246 5.636 11.293L12.472 4.458C13.774 3.156 15.884 3.156 17.186 4.458C18.488 5.759 18.488 7.87 17.186 9.172L10.361 15.996C9.71 16.647 8.655 16.647 8.004 15.996C7.353 15.345 7.353 14.29 8.004 13.639L14.226 7.418" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                                     </svg>
-                                    <span class="w-20 sm:w-full text-sm">Attach Media</span>
+                                    <span class="text-sm">Attach Media</span>
                                 </label>
                                 <input type="file" name="media[]" id="image" class="hidden" multiple onchange="updateFileList()">
                             </div>
